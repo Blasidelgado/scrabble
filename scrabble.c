dd190a7667b1843b009143887d529a4918f6ba1c
Blasidelgado <blasidelgado@gmail.com>
2023-07-02 16:23:18 -0300

    add cs50x solution

--- scrabble.c
+++ scrabble.c
@@ -14,14 +14,76 @@ int main(void)
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
-    // Score both words
+    // Store score for both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Evaluate winner and print
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    // Initialize score
+    int score = 0;
+
+    // Iterate through word to check individual characters
+    for (int letter = 0; letter < strlen(word); letter++)
+    {
+        // Evaluate character for points
+        switch (toupper(word[letter])) // Ignore lowercase letters
+        {
+            case 65:
+            case 69:
+            case 73:
+            case 76:
+            case 78:
+            case 79:
+            case 82:
+            case 83:
+            case 84:
+            case 85:
+                score++;
+                break;
+            case 68:
+            case 71:
+                score += 2;
+                break;
+            case 66:
+            case 67:
+            case 77:
+            case 80:
+                score += 3;
+                break;
+            case 70:
+            case 86:
+            case 87:
+            case 89:
+                score += 4;
+                break;
+            case 75:
+                score += 5;
+                break;
+            case 81:
+            case 90:
+                score += 10;
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Return total word score
+    return score;
 }
